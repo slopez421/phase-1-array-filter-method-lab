6f2e28b85fef70c5d9674b69a8ad1b9ee20ca210
Shania Lopez <slopez@fandm.edu>
2023-04-19 06:18:56 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+// function findMatching(drivers) {
+//     const names = name.filter(name => name === "name")
+//     if(name){
+//     return names}
+// }
+
+function findMatching(drivers, singleDriver) {
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === singleDriver.toLowerCase() 
+    })
+  } debugger
+
+  function fuzzyMatch(drivers, singleDriver) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, singleDriver.length) === singleDriver.toLowerCase()
+    })
+  }
+
+  function matchName(drivers, singleDriver) {
+    return drivers.filter(function (driver) {
+        return driver.name === singleDriver
+    })
+
+  }
